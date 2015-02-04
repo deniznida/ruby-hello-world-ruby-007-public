3b7cabbd5d5b1c3193f3efe37c04cd1ac38014c5
Ville Vesterinen <villevesterinen@gmail.com>
2015-02-04 13:33:44 -0500

    all done

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -1,9 +1,10 @@
-describe "Hello World" do 
+describe "Hello World" do
 
   it 'puts "Hello World!"' do
     # $stdout represents the output of your program, don't worry about how
     # we are testing this, focus on simply coding your solution.
     expect($stdout).to receive(:puts).with("Hello World!")
+    puts "Hello World!"
     # Get your program to puts "Hello World!"
     # Code your solution right here.
   end
